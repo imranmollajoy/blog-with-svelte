95483421f2abfc1f760f4452f6139a1363536c22
Imran Molla <imranmollajoy@gmail.com>
2022-07-11 14:25:17 +0600

    updated contentfulclinet

--- src/lib/contentfulClient.js
+++ src/lib/contentfulClient.js
@@ -1,6 +1,14 @@
 import contentful from 'contentful';
-
-export const client = contentful.createClient({
-	space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
-	accessToken: import.meta.env.VITE_CONTENTFUL_DELIVERY_API_TOKEN
-});
+let clientData = {};
+if (process.env.NODE_ENV === 'production') {
+	clientData = {
+		space: process.env.CONTENTFUL_SPACE_ID,
+		accessToken: process.env.CONTENTFUL_DELIVERY_API_TOKEN
+	};
+} else {
+	clientData = {
+		space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+		accessToken: import.meta.env.VITE_CONTENTFUL_DELIVERY_API_TOKEN
+	};
+}
+export const client = contentful.createClient(clientData);
